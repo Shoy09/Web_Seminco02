c37abafde6f1b7b74b805631ba8d0c4588ba3237
Shoy09 <jhoel.dioses@tecsup.edu.pe>
2025-02-20 08:51:12 -0500

    agregar rutas y api

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app/app.routes';
+import { provideZoneChangeDetection } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AuthInterceptor } from './app/Components/services/auth-interceptor.service';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),
-    provideHttpClient()  // 🔹 Agregar HttpClient como proveedor
-  ],
+      provideZoneChangeDetection({ eventCoalescing: true }),
+      provideRouter(routes),
+      provideAnimations(),
+      provideHttpClient(withInterceptorsFromDi()), // Habilita el uso de interceptores
+      { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    ]
 }).catch(err => console.error(err));
